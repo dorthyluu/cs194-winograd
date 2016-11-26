211f253f1f6a64b045cc7009ab583ef555da638f
Michael Cheng <mikehcheng@gmail.com>
2016-11-26 06:53:23 -0800

    winograd uses cubes where possible, does not take in N

--- winograd.cpp
+++ winograd.cpp
@@ -8,12 +8,12 @@ using namespace std;
 using namespace arma;
 
 double timestamp();
-void report_winograd_statistics(int K, int C, int P, int N, double time);
+void report_winograd_statistics(int K, int C, int P, double time);
 
 mat** create_fourd_array(int d1, int d2, int d3, int d4) {
-  mat** array = (mat**) malloc(sizeof(mat*) * d1);
+  mat** array = new mat*[d1]();
   for (int i = 0; i < d1; i++) {
-    array[i] = (mat*) malloc(sizeof(mat) * d2);
+    array[i] = new mat[d2]();
     for (int j = 0; j < d2; j++) {
       array[i][j] = mat(d3, d4);
     }
@@ -23,12 +23,12 @@ mat** create_fourd_array(int d1, int d2, int d3, int d4) {
 
 void free_fourd_array(mat** array, int d1) {
   for (int i = 0; i < d1; i++) {
-    free(array[i]);
+    delete[] array[i];
   }
-  free(array);
+  delete[] array;
 }
 
-mat** convolute(int K, int C, int H, int W, int N, mat** filters, mat** images) {
+void convolute(int K, int C, int H, int W, cube* filters, cube& image, cube& result) {
   int m = 2;
   int r = 3;
   int alpha = m + r - 1;
@@ -36,7 +36,7 @@ mat** convolute(int K, int C, int H, int W, int N, mat** filters, mat** images) 
   int out_W = W - r + 1;
   int num_h_tiles = ceil(out_H/m);
   int num_w_tiles = ceil(out_W/m);
-  int P = N * num_h_tiles * num_w_tiles;
+  int P = num_h_tiles * num_w_tiles;
   mat G = { {1.0, 0.0, 0.0},
             {0.5, 0.5, 0.5},
             {0.5, -0.5, 0.5},
@@ -50,25 +50,25 @@ mat** convolute(int K, int C, int H, int W, int N, mat** filters, mat** images) 
             {1, -1},
             {0, -1}};
 
-  auto gen_b = [num_h_tiles, num_w_tiles](int i, int y, int x) -> int {
-    return i * num_h_tiles * num_w_tiles + y * num_w_tiles + x;
+  auto gen_b = [num_h_tiles, num_w_tiles](int y, int x) -> int {
+    return y * num_w_tiles + x;
   };
 
   // factoring out malloc'ing before measuring runtime
   mat **U = create_fourd_array(alpha, alpha, K, C);
   mat **V = create_fourd_array(alpha, alpha, C, P);
-  mat **M = (mat**) malloc(sizeof(mat*) * alpha);
-  mat **Y = create_fourd_array(N, K, out_H, out_W);
+  mat**M = new mat*[alpha]();
   for (int xi = 0; xi < alpha; xi++) {
-    M[xi] = (mat*) malloc(sizeof(mat) * alpha);
+    M[xi] = new mat[alpha];
   }
+  
 
   double time = timestamp();
 
   for (int k = 0; k < K; k++) {
     for (int c = 0; c < C; c++) {
       // flop: K * C * (4 * 3 * 5) * 2 
-      mat u = G * filters[k][c] * G.t();
+      mat u = G * filters[k].slice(c) * G.t();
       for (int xi = 0; xi < alpha; xi++) {
         for (int nu = 0; nu < alpha; nu++) {
           U[xi][nu](k, c) = u(xi, nu);
@@ -77,24 +77,22 @@ mat** convolute(int K, int C, int H, int W, int N, mat** filters, mat** images) 
     }
   }
 
-  for (int i = 0; i < N; i++) {
-    for (int c = 0; c < C; c++) {
-      for (int y = 0; y < num_h_tiles; y++) {
-        for (int x = 0; x < num_w_tiles; x++) {
-          mat d = images[i][c](span(y * m, y * m + alpha - 1), span(x * m, x * m + alpha - 1));
-          // flop: C * P * (4 * 4 * 7) * 2
-          mat v = B.t() * d * B;
-          int b = gen_b(i, y, x);
-          for (int xi = 0; xi < alpha; xi++) {
-            for (int nu = 0; nu < alpha; nu++) {
-              V[xi][nu](c, b) = v(xi, nu);
-            }
+  for (int c = 0; c < C; c++) {
+    mat channel = image.slice(c);
+    for (int y = 0; y < num_h_tiles; y++) {
+      for (int x = 0; x < num_w_tiles; x++) {
+        mat d = channel(span(y * m, y * m + alpha - 1), span(x * m, x * m + alpha - 1));
+        // flop: C * P * (4 * 4 * 7) * 2
+        mat v = B.t() * d * B;
+        int b = gen_b(y, x);
+        for (int xi = 0; xi < alpha; xi++) {
+          for (int nu = 0; nu < alpha; nu++) {
+            V[xi][nu](c, b) = v(xi, nu);
           }
         }
       }
     }
   }
-
   
   for (int xi = 0; xi < alpha; xi++) {
     for (int nu = 0; nu < alpha; nu++) {
@@ -104,30 +102,27 @@ mat** convolute(int K, int C, int H, int W, int N, mat** filters, mat** images) 
   }
 
   mat m_hold = zeros<mat>(alpha, alpha);
-  for (int i = 0; i < N; i++) {
-    for (int k = 0; k < K; k++) {
-      for (int y = 0; y < num_h_tiles; y++) {
-        for (int x = 0; x < num_w_tiles; x++) {
-          int b = gen_b(i, y, x);
-          for (int xi = 0; xi < alpha; xi++) {
-            for (int nu = 0; nu < alpha; nu++) {
-              m_hold(xi, nu) = M[xi][nu](k, b);
-            }
+  for (int k = 0; k < K; k++) {
+    for (int y = 0; y < num_h_tiles; y++) {
+      for (int x = 0; x < num_w_tiles; x++) {
+        int b = gen_b(y, x);
+        for (int xi = 0; xi < alpha; xi++) {
+          for (int nu = 0; nu < alpha; nu++) {
+            m_hold(xi, nu) = M[xi][nu](k, b);
           }
-          // flop: N * K * P * (2 * 4 * 7) * 2
-          Y[i][k](span(y*m, (y+1)*m-1), span(x*m, (x+1)*m-1)) = A.t() * m_hold * A;
         }
+        // flop: K * P * (2 * 4 * 7) * 2
+        result.slice(k)(span(y*m, (y+1)*m-1), span(x*m, (x+1)*m-1)) = A.t() * m_hold * A;
       }
     }
   }
 
   time = timestamp() - time;
-  report_winograd_statistics(K, C, P, N, time);
+  report_winograd_statistics(K, C, P, time);
 
   free_fourd_array(U, alpha);
   free_fourd_array(V, alpha);
   free_fourd_array(M, alpha);
-  return Y;
 }
 
 double timestamp()
@@ -137,11 +132,11 @@ double timestamp()
   return tv.tv_sec + 1e-6*tv.tv_usec;
 }
 
-void report_winograd_statistics(int K, int C, int P, int N, double time) {
+void report_winograd_statistics(int K, int C, int P, double time) {
   int flop = (K * C * (4 * 3 * 5) * 2 +
-                   C * P * (4 * 4 * 7) * 2 + 
-                   16 * K * P * (2 * C - 1) + 
-                   N * K * P * (2 * 4 * 7) * 2);
+              C * P * (4 * 4 * 7) * 2 + 
+              16 * K * P * (2 * C - 1) + 
+              K * P * (2 * 4 * 7) * 2);
   double mflops = flop / (1024.0 * 1024.0 * time);
   cout << "Floating point operations: " << flop << "\n";
   cout << "Time Elapsed: " << time << "\n";
@@ -155,46 +150,41 @@ int main(int argc, char* argv[])
   }
   ifstream file;
   file.open(argv[1]);
-  int K, C, H, W, N;
-  file >> K >> C >> H >> W >> N;
-  mat** filters = create_fourd_array(K, C, 3, 3);
+  int K, C, H, W;
+  file >> K >> C >> H >> W;
+  cube* filters = new cube[K]();
   for (int i = 0; i < K; i++) {
+    filters[i] = cube(3, 3, C);
     for (int j = 0; j < C; j++) {
       for (int row = 0; row < 3; row++) {
         for (int col = 0; col < 3; col ++) {
-          file >> filters[i][j](row, col);
+          file >> filters[i](row, col, j);
         }
       }
     }
   }
 
-  mat** images = create_fourd_array(N, C, H, W);
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < C; j++) {
-      for (int row = 0; row < H; row++) {
-        for (int col = 0; col < W; col ++) {
-          file >> images[i][j](row, col);
-        }
+  cube image = cube(H, W, C);
+  for (int c = 0; c < C; c++) {
+    for (int row = 0; row < H; row++) {
+      for (int col = 0; col < W; col++) {
+        file >> image(row, col, c);
       }
     }
   }
   file.close();
 
-  mat** Y = convolute(K, C, H, W, N, filters, images);
+  cube result = cube(H-3+1, W-3+1, K);
+  convolute(K, C, H, W, filters, image, result);
 
   ofstream fileout;
   fileout.open(argv[2], ofstream::out | ofstream::trunc );
-  fileout << K << " " << C << " " << H << " " << W << " " << N << endl;
-  for (int i = 0; i < N; i++) {
-    for (int k = 0; k < K; k++) {
-      fileout << Y[i][k] << "\n";
-    }
-    fileout << "\n";
+  fileout << K << " " << C << " " << H << " " << W << endl;
+  for (int i = 0; i < K; i++) {
+    fileout << result.slice(i) << "\n";
   }
   fileout.close();
 
-  free_fourd_array(Y, N);
-  free_fourd_array(filters, K);
-  free_fourd_array(images, N);
+  delete[] filters;
   return 0;
 }
