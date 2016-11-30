9e71b2207bc2679d0e92b9ff2051aa5d789ce607
Dorthy Luu <dluu@berkeley.edu>
2016-11-29 20:55:01 -0800

    increasing image size

--- bench_channels.sh
+++ bench_channels.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "K C H W" >> bench_channels.txt
-for n in {0..10};
+for n in {0..5};
 do 
     C=$((1 << n))
-    echo 64 $((C)) 256 256 >> bench_channels.txt
-    python3 gen_problem.py 64 $((C)) 256 256 > 64_$((C))_256_256.in
-    ./naive_convolution 64_$((C))_256_256.in naive_64_$((C))_256_256.out >> bench_channels.txt
-    ./winograd_gpu 64_$((C))_256_256.in gpu_64_$((C))_256_256.out >> bench_channels.txt
+    echo 64 $((C)) 512 512 >> bench_channels.txt
+    python3 gen_problem.py 64 $((C)) 512 512 > 64_$((C))_512_512.in
+    ./naive_convolution 64_$((C))_512_512.in naive_64_$((C))_512_512.out >> bench_channels.txt
+    ./winograd_gpu 64_$((C))_512_512.in gpu_64_$((C))_512_512.out >> bench_channels.txt
 
-    echo 64 $((C)) 256 256
-    ./compare_outputs naive_64_$((C))_256_256.out gpu_64_$((C))_256_256.out
+    echo 64 $((C)) 512 512
+    ./compare_outputs naive_64_$((C))_512_512.out gpu_64_$((C))_512_512.out
 done
--- bench_filters.sh
+++ bench_filters.sh
@@ -3,12 +3,12 @@ echo "K C H W" >> bench_filters.txt
 for n in {0..9};
 do 
     K=$((1 << n))
-    echo $((K)) 3 256 256 >> bench_filters.txt
-    python3 gen_problem.py $((K)) 3 256 256 > $((K))_3_256_256.in
-    ./naive_convolution $((K))_3_256_256.in naive_$((K))_3_256_256.out >> bench_filters.txt
-    ./winograd_gpu $((K))_3_256_256.in gpu_$((K))_3_256_256.out >> bench_filters.txt
+    echo $((K)) 3 512 512 >> bench_filters.txt
+    python3 gen_problem.py $((K)) 3 512 512 > $((K))_3_512_512.in
+    ./naive_convolution $((K))_3_512_512.in naive_$((K))_3_512_512.out >> bench_filters.txt
+    ./winograd_gpu $((K))_3_512_512.in gpu_$((K))_3_512_512.out >> bench_filters.txt
 
-    echo $((K)) 3 256 256
-    ./compare_outputs naive_$((K))_3_256_256.out gpu_$((K))_3_256_256.out
+    echo $((K)) 3 512 512
+    ./compare_outputs naive_$((K))_3_512_512.out gpu_$((K))_3_512_512.out
 
 done
