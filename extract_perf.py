473ff15f5e3807976678d5e46940e23a7a16fbf5
Takeshi Mochida <takeshimochida@gmail.com>
2016-11-30 00:44:18 -0800

    Extract script: record time as well

--- extract_perf.py
+++ extract_perf.py
@@ -15,32 +15,44 @@ if (argc != 2):
 # skip header line
 lines = open(sys.argv[1]).read().split("\n")[1:]
 
-out = open("bench_extract.csv", "w+")
-out.write("K,C,H,W,naive,winograd,winograd_openmp,winograd_gpu\n")
+out_flop = open("bench_extract_flops.csv", "w+")
+out_time = open("bench_extract_times.csv", "w+")
+
+# write CSV headers
+out_flop.write("K,C,H,W,naive,winograd,winograd_openmp,winograd_gpu\n")
+out_time.write("K,C,H,W,naive,winograd,winograd_openmp,winograd_gpu\n")
 
 index = 0
 while index < len(lines) - 1:
   try :
     K, C, H, W = lines[index].split(" ")
+    index += 1
   except Exception:
     print("Unexpected format")
     sys.exit(-1)
-  index += 1
+
   mflops = []
+  times = []
   for i in range(BENCH_PER_SIZE * LINES_PER_BENCH):
     line = lines[index + i]
+    if "Time" in line:
+      times.append(line.split(" ")[-1])
     if "MFlop" in line:
       mflops.append(line.split(" ")[-1])
 
-  if len(mflops) != BENCH_PER_SIZE:
+  if len(mflops) != BENCH_PER_SIZE or len(times) != BENCH_PER_SIZE:
     print("Benchmark output missing values")
     sys.exit(-1)
 
-  outline = "{},{},{},{},".format(K, C, H, W)
-  for mflop in mflops:
-    outline += mflop + ","
+  outline_flop = "{},{},{},{},".format(K, C, H, W)
+  outline_time = "{},{},{},{},".format(K, C, H, W)
+  for i in range(len(mflops)):
+    outline_flop += mflops[i] + ","
+    outline_time += times[i] + ","
 
-  out.write(outline[:-1] + "\n")
+  out_flop.write(outline_flop[:-1] + "\n")
+  out_time.write(outline_time[:-1] + "\n")
   index += BENCH_PER_SIZE * LINES_PER_BENCH
 
-out.close()
+out_flop.close()
+out_time.close()
