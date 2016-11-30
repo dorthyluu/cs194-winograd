1a8a33a712dac4b054c43b61631968b4ec87f900
Michael Cheng <mikehcheng@gmail.com>
2016-11-29 23:33:33 -0800

    Updated benchmarking scripts to include openmp and base winograd

--- bench_filters.sh
+++ bench_filters.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Running..."
 echo "K C H W" >> bench_filters.txt
 for n in {0..7};
 do 
@@ -6,9 +7,11 @@ do
     echo $((K)) 3 512 512 >> bench_filters.txt
     python3 gen_problem.py $((K)) 3 512 512 > $((K))_3_512_512.in
     ./naive_convolution $((K))_3_512_512.in naive_$((K))_3_512_512.out >> bench_filters.txt
+    ./winograd $((K))_3_512_512.in win_$((K))_3_512_512.out >> bench_filters.txt
+    ./winograd_openmp $((K))_3_512_512.in openmp_$((K))_3_512_512.out >> bench_filters.txt
     ./winograd_gpu $((K))_3_512_512.in gpu_$((K))_3_512_512.out >> bench_filters.txt
-
-    echo $((K)) 3 512 512
-    ./compare_outputs naive_$((K))_3_512_512.out gpu_$((K))_3_512_512.out
-
 done
+echo "Comparing outputs..."
+./compare_outputs naive_$((K))_3_512_512.out win_$((K))_3_512_512.out
+./compare_outputs naive_$((K))_3_512_512.out openmp_$((K))_3_512_512.out
+./compare_outputs naive_$((K))_3_512_512.out gpu_$((K))_3_512_512.out
